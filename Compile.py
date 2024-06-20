0b222c1659a765cce37f7d09bfaad3f411a10544
Thomas Dollé <td78220@icloud.com>
2024-06-20 19:53:32 +0200

    merged struct & printf(string)

--- Compile.py
+++ Compile.py
@@ -3,62 +3,131 @@ import lark
 global cpt
 cpt = 0
 
-op2asm = {"+": "add rax, rbx", "-": "sub rax, rbx"}
-
 def compile(ast):
     asmString = ""
-    asmString += "extern printf, atol, puts ; déclaration des fonctions externes\n"
-    asmString += "global main ; déclaration main\n"
-    asmString += "section .data ; section des données\n"
-    asmString += "long_format: db '%lld', 10, 0 ; format pour les int64_t\n"
-    asmString += "format : db '%s', 0 ; format pour les chaînes\n"
-    asmString += "argc : dq 0 ; copie de argc\n"
-    asmString += "argv : dq 0 ; copie de argv\n"
-    asmString += "buffer: times 100 db 0 ; buffer pour la concaténation de chaînes\n\n;Declaration vars\n"
-    asmVar, vars = variable_declaration(ast.children[0])
-    asmString += asmVar
-    asmString += "section .text ; instructions\n"
-    asmString += "main :\n"
+    asmString = asmString + "extern printf, atoi ; declaration des fonctions externes\n"
+    asmString = asmString + "global main ; declaration main\n"
+    asmString = asmString + "section .data ; section des donnees\n"
+    asmString = asmString + "long_format: db '%lld', 10, 0 ; format pour les int64_t\n"
+    asmString += "format : db '%s', 0 ; format pour les chaînes\n" 
+    asmString = asmString + "argc : dq 0 ; copie de argc\n"
+    asmString = asmString + "argv : dq 0 ; copie de argv\n"
+    asmString += "buffer: times 100 db 0 ; buffer pour la concaténation de chaînes\n\n;Declaration vars\n" 
+    asmVar, vars = variable_declaration(ast.children[1])
+    asmVarLoc, varsLoc = variable_locales_declaration(vars, ast.children[0], ast.children[2])
+    asmVar = asmVar + asmVarLoc
+    asmString = asmString + asmVar
+    asmString = asmString + "section .text ; instructions\n"
+    asmString = asmString + "main :\n"
     asmString += "push rbp; Set up the stack. Save rbp\n"
-    asmString += "mov rbp, rsp ; Save rbp\n"
+    asmString += "mov rbp, rsp ; Save rbp\n" 
     asmString += "mov [argc], rdi\n"
-    asmString += "mov [argv], rsi\n\n;Init vars\n"
-    asmString += initMainVar(ast.children[0])+ "\n;Commandes:\n"
-    asmString += compilCommand(ast.children[1])+ "\n;Return:\n"
-    asmString += compilReturn(ast.children[2])
+    asmString += "mov [argv], rsi\n"
+    asmString += initMainVar(vars)+ "\n;Commandes:\n"
+    vars = vars | varsLoc
+    asmString += compilCommand(ast.children[2], vars) + "\n;Return:\n"
+    asmString += compilReturn(ast.children[3])
     asmString += "pop rbp\n"
     asmString += "xor rax, rax\n"
     asmString += "ret\n"
     return asmString
 
+# Ici, on s'occupe des variables dans les arguments de la fonction main
 def variable_declaration(ast):
     asmVar = ""
-    vars = set()
+    vars = dict()
     if ast.data != "liste_vide":
         for child in ast.children:
-            if child.value.startswith('s'):
-                asmVar += f"{child.value}: times 100 db 0\n"
-            else:
-                asmVar += f"{child.value}: dq 0\n"
-            vars.add(child.value)
-    print(vars)
+            asmVar += f"{child.value}: dq 0\n"
+            #vars[child.value] = "int" #Dinojan
+            vars[child.value] = f"{child.type}"
     return asmVar, vars
 
-def initMainVar(ast):
+# On cherche les déclarations de variables locales dans les commandes
+def cherche_variables_locales(tree):
+    if tree.data in ("com_struct_declaration", "com_int_declaration"):
+        return [tree]
+    elif tree.data == "com_sequence":
+        commands = []
+        for child in tree.children:
+            commands += cherche_variables_locales(child)
+        return commands
+    elif tree.data == "com_if":
+        return cherche_variables_locales(tree.children[1]) + cherche_variables_locales(tree.children[2])
+    elif tree.data == "com_while":
+        return cherche_variables_locales(tree.children[1])  
+    return []
+
+# On crée les variables locales
+def variable_locales_declaration(varDict, structDeclar, commandes):
+
+    structDict = dict()
+    varLocalDict = dict()
+
+    # On stocke les attributs des structs dans un dictionnaire
+    if structDeclar.data == "liste_def_struct_normale":
+        for childStruct in structDeclar.children:
+            structName = childStruct.children[0].value
+            if childStruct.children[1].data != "liste_attribut_vide":
+                attributes =  []
+                for child in childStruct.children[1].children:
+                    attributes.append(child.value)
+                structDict[structName] = attributes
+
+    # On crée les variables locales (int et struct)
     asmVar = ""
-    if ast.data != "liste_vide":
-        index = 0
-        for child in ast.children:
-            asmVar += "mov rbx, [argv]\n"
-            asmVar += f"mov rdi, [rbx + { 8*(index+1)}]\n"
-            if child.type == "VARIABLE":
-                asmVar += "xor rax, rax\n"
-                asmVar += "call atol\n"
-                asmVar += f"mov [{child.value}], rax\n"
-            elif child.type == "VARIABLE_STRING":
-                asmVar += f"mov [{child.value}], rdi\n"
-            index += 1
+    commands = cherche_variables_locales(commandes)
+    for command in commands:
+        if command.data == "com_int_declaration":
+            for child in command.children:
+                if child.value in varDict:
+                    raise Exception(f"Variable {child.value} already declared")
+                else:  
+                    asmVar += f"{child.value}: dq 0\n"
+                    varLocalDict[child.value] = "int"
+        elif command.data == "com_struct_declaration":
+            name = command.children[0].value
+            info = name.split("_")
+            structName, varName = info[1], info[2]
+            if structName not in structDict:
+                raise Exception(f"Struct {structName} does not exist in struct {structName}")
+            if varName not in varDict:
+                raise Exception(f"Variable {varName} does not exist in struct {structName}")
+            for attribute in structDict[structName]:
+                varLocalDict[f"{name}"] = "struct"
+                asmVar += creer_variables_asm(name, attribute, structDict, varLocalDict)
+    return asmVar, varLocalDict
+
+# Fonction pour créer les variables locales des structs (récursivement)
+def creer_variables_asm(name, attribute, structDict, varLocalDict):
+    attributeInfo = attribute.split()
+    if attributeInfo[0] in ("string", "int"):
+        varLocalDict[f"{name}_{attributeInfo[1]}"] = attributeInfo[0]
+        return f"{name}_{attributeInfo[1]}: dq 0\n"
+    else: 
+        asm = ""
+        varLocalDict[f"{name}_{attributeInfo[2]}"] = attributeInfo[0]
+        for attributeStruct in structDict[attributeInfo[1]]:
+            newName = f"{name}_{attributeInfo[2]}"
+            asm += creer_variables_asm(newName, attributeStruct, structDict, varLocalDict)
+        return asm
+
+# On initialise les variables des arguments de la fonction main
+def initMainVar(vars):
+    asmVar = ""
+    index = 0
+    for key, _ in vars.items():
+        asmVar += "mov rbx, [argv]\n"
+        asmVar += f"mov rdi, [rbx + { 8*(index+1)}]\n"
+        if vars[key] == "VARIABLE":
+            asmVar += "xor rax, rax\n"
+            asmVar += "call atoi\n"
+            asmVar += f"mov [{key}], rax\n"
+        elif vars[key] == "VARIABLE_STRING":
+            asmVar += f"mov [{key}], rdi\n"
+        index += 1
     return asmVar
+
 
 def compilReturn(ast):
     asm = compilExpression(ast)
@@ -70,47 +139,52 @@ def compilReturn(ast):
     asm += "call printf \n"
     return asm
 
-def compilCommand(ast):
+def compilCommand(ast, vars):
     asmVar = ""
     if ast.data == "com_while":
-        asmVar = compilWhile(ast)
+        asmVar = compilWhile(ast, vars)
     elif ast.data == "com_if":
-        asmVar = compilIf(ast)
+        asmVar = compilIf(ast, vars)
     elif ast.data == "com_sequence":
-        asmVar = compilSequence(ast)
-    elif ast.data == "com_asgt":
+        asmVar = compilSequence(ast, vars)
+    elif ast.data == "com_assignation":
         asmVar = compilAsgt(ast)
     elif ast.data == "com_printf":
         asmVar = compilPrintf(ast)
+    elif ast.data == "com_assignation_struct_attribut":
+        asmVar = compilAssignationStructAttribut(ast, vars)
     return asmVar
 
-def compilWhile(ast):
+def compilWhile(ast, vars):
     global cpt
     cpt += 1
     return f""" 
             loop{cpt} : {compilExpression(ast.children[0])}
                 cmp rax, 0
                 jz fin{cpt}
-                {compilCommand(ast.children[1])}
+                {compilCommand(ast.children[1], vars)}
                 jmp loop{cpt}
             fin{cpt} :
         """
 
-def compilIf(ast):
+def compilIf(ast, vars):
     global cpt
     cpt += 1
     return f""" 
             {compilExpression(ast.children[0])}
             cmp rax, 0
-            jz fin{cpt}
-            {compilCommand(ast.children[1])}
+            jz else{cpt}
+            {compilCommand(ast.children[1], vars)}
+            jmp fin{cpt}
+            else{cpt} :
+            {compilCommand(ast.children[2], vars)}
             fin{cpt} :
         """
 
-def compilSequence(ast):
+def compilSequence(ast, vars):
     asm = ""
-    for child in ast.children:
-        asm += compilCommand(child)
+    for child in ast.children :
+        asm += compilCommand(child, vars)
     return asm
 
 def compilAsgt(ast):
@@ -118,13 +192,34 @@ def compilAsgt(ast):
     asm += f"mov [{ast.children[0].value}], rax \n"
     return asm
 
+# On compile les assignations de variables de structs en vérifiant le type des variables
+def compilAssignationStructAttribut(ast, vars):
+    transform = ast.children[0].value.replace(".", "_")
+    asm = ""
+    if vars[transform] == "int" and ast.children[1].data != "exp_struct":
+        asm = compilExpression(ast.children[1])
+        asm += f"mov [{transform}], rax \n"
+    elif vars[transform] == "int" and vars[ast.children[1].children[0].value] == "int":
+        asm = compilExpression(ast.children[1])
+        asm += f"mov [{transform}], rax \n"
+    elif vars[transform] == "struct" and vars[ast.children[1].children[0].value] == "struct":
+        for key, value in vars.items():
+            if key.startswith(transform) and value == "int":
+                suffix = key[len(transform):]
+                asm += f"mov rax, [{ast.children[1].children[0].value + suffix}]\n"
+                asm += f"mov [{key}], rax\n"
+    return asm
+
 def compilPrintf(ast):
-    #print le contenu de rax
+    if ast.data == "exp_struct_attribut":
+        transform = ast.children[0].value.replace(".", "_")
+        ast.children[0].value = transform
     asm = compilExpression(ast.children[0])
+    asm += "mov rsi, rax \n"
     if ast.children[0].data == "exp_variable_string" or ast.children[0].data == "exp_string":
-        asm += f"mov rsi, rax\nmov rdi, format\n"
+        asm += f"mov rdi, format\n"
     elif ast.children[0].data == "exp_variable" or ast.children[0].data == "exp_nombre":
-        asm += f"mov rsi, rax\nmov rdi, long_format\n"
+        asm += f"mov rdi, long_format\n"
     asm += "xor rax, rax \n"
     asm += "call printf \n"
     return asm
@@ -134,130 +229,62 @@ def compilExpression(ast):
         return f"mov rax, [{ast.children[0].value}]\n"
     elif ast.data == "exp_variable":
         return f"mov rax, [{ast.children[0].value}]\n"
-    elif ast.data ==  "exp_nombre":
-        return f"mov rax, {ast.children[0].value}\n"
     elif ast.data ==  "exp_string":
         return f"mov rax, {ast.children[0].value}\n"
+    elif ast.data ==  "exp_nombre":
+        return f"mov rax, {ast.children[0].value}\n"
+    elif ast.data == "exp_struct":
+        ast.children[0].value = ast.children[0].value.replace(".", "_")
+        return f"mov rax, [{ast.children[0].value}]\n"
+    elif ast.data == "exp_struct_attribut":
+        ast.children[0].value = ast.children[0].value.replace(".", "_")
+        print(f"mov rax, [{ast.children[0].value}]\n")
+        return f"mov rax, [{ast.children[0].value}]\n"
     elif ast.data == "exp_binaire":
-        asm = ""
-        if ast.children[0].data == "exp_variable" and ast.children[2].data == "exp_variable":
-            asm += f"""
-    {compilExpression(ast.children[2])}
-    push rax
-    {compilExpression(ast.children[0])}
-    pop rbx
-    """
-            asm += "add rax, rbx\n"
-        else:
-            asm += compilAddStrings(ast)
-        return asm
-    elif ast.data == "exp_charlen":
-        asm = compilCharLen(ast)
-        return asm
-    elif ast.data == "exp_charat":
-        asm = compilCharAt(ast)
-        return asm
-    return f"; expression non reconnue{ast.data}\n"
-
-# Fonction d'assembly pour `charAt`
-def compilCharAt(ast):
-    if ast.children[1].type == "NOMBRE":
-        asmVar = f"""
-            mov     rdi, [{ast.children[0]}]
-            mov     rsi, {ast.children[1]}
-            """
-    elif ast.children[1].type == "VARIABLE":
-        asmVar = f"""
-        mov     rdi, [{ast.children[0]}]
-        mov     rsi, [{ast.children[1]}]
-            """
-    global cpt
-    cpt+=1
-    asmVar += f"""
-        mov     rdx, rdi
-        mov     rcx, rsi
-        mov     rax, 0 ; initialise l'iterator
-        loop_start{cpt}:
-        cmp     byte [rdx + rax], 0 ; null terminator
-        je      loop_end{cpt}
-        inc     rax
-        cmp     rax, rcx        ; stop si counter = n
-        je      loop_end{cpt}
-        jmp     loop_start{cpt}
-        loop_end{cpt}:
-        movzx   eax, byte [rdx + rax - 1] ; nieme char dans rax
-        
-        mov rsi,rax
-        mov rdi, long_format
-        """
-    return asmVar
-
-def compilCharLen(ast): #returns the len in rsi
-    global cpt
-    cpt+=1
-    return f"""
-        mov rdi, [{ast.children[0]}]
-        mov     rdx, rdi
-        mov     rcx, rsi
-        mov     rax, 0 ; initialise l'iterator
-        loop_start{cpt}:
-        cmp     byte [rdx + rax], 0 ; null terminator
-        je      loop_end{cpt}
-        inc     rax
-        jmp loop_start{cpt}
-        loop_end{cpt}:
-        mov rsi,rax
-        mov rdi, long_format
-    """
-
-def compilAddStrings(ast):
-    global cpt
-    cpt += 3
-    asm = f"""
-    mov [argc], rdi
-    mov [argv], rsi
-
-    mov rbx, rsi
-
-    ; Copy str1 to str3
-    mov rsi, [{ast.children[0].children[0]}]    ; rsi pointe maintenant sur argv[1]
-    mov rdi, str3         ; Destination str3
-    copy_loop{cpt-2}:
-        lodsb            ; Load byte from rsi into al
-        stosb            ; Store byte in al to rdi
-        test al, al      ; Check if the byte is 0 (end of string)
-        jnz copy_loop{cpt-2}   ; If not 0, continue the loop
-        
-    ; Append str2 to str3
-    """
-    asm += f"""
-    ;Calculate length
-    mov rdi, [{ast.children[0].children[0]}]
-    mov     rdx, rdi
-    mov     rcx, rsi
-    mov     rax, 0 ; initialise l'iterator
-    loop_start{cpt-1}:
-    cmp     byte [rdx + rax], 0 ; null terminator
-    je      loop_end{cpt-1}
-    inc     rax
-    jmp loop_start{cpt-1}
-    loop_end{cpt-1}:
-    """
-    asm += f"""
-    ;Concatenate
-    mov rsi, [{ast.children[2].children[0]}]        ; Source string 2
-    mov rdi, str3       ; Destination string 3
-    add rdi, rax       ; Move rdi to the end of str1 in str3
-    copy_loop{cpt}:
-        lodsb            ; Load byte from rsi into al
-        stosb            ; Store byte in al to rdi
-        test al, al      ; Check if the byte is 0 (end of string)
-        jnz copy_loop{cpt}   ; If not 0, continue the loop
-    
-    mov rax, str3
-    mov rsi, rax
-    mov rdi, format
-    xor rax, rax 
-    call printf ;print le resultat, ne supporte pas l'assignation dans rax par ex (pas compatible avec le printf tel quel)
-    """
-    return asm
+        if ast.children[1].value == "+":
+            return f"""
+                    {compilExpression(ast.children[2])}
+                    push rax
+                    {compilExpression(ast.children[0])}
+                    pop rbx
+                    add rax, rbx
+                    """
+        elif ast.children[1].value == "-":
+            return f"""
+                    {compilExpression(ast.children[2])}
+                    push rax
+                    {compilExpression(ast.children[0])}
+                    pop rbx
+                    sub rax, rbx
+                    """
+        elif ast.children[1].value == "*":
+            return f"""
+                    {compilExpression(ast.children[2])}
+                    push rax
+                    {compilExpression(ast.children[0])}
+                    pop rbx
+                    imul rax, rbx
+                    """
+        elif ast.children[1].value == ">":
+            return f"""
+                    {compilExpression(ast.children[2])}
+                    push rax
+                    {compilExpression(ast.children[0])}
+                    pop rbx
+                    cmp rax, rbx
+                    setg al
+                    movzx rax, al
+                    """
+        elif ast.children[1].value == "<":
+            return f"""
+                    {compilExpression(ast.children[2])}
+                    push rax
+                    {compilExpression(ast.children[0])}
+                    pop rbx
+                    cmp rax, rbx
+                    setl al
+                    movzx rax, al
+                    """
+    else: 
+        return f";expression inconnue {ast}"
+    return ""
--- Parser.py
+++ Parser.py
@@ -6,13 +6,19 @@ grammaire = """
 %import common.WS  // Importation des espaces blancs.
 %ignore WS  // Ignorer les espaces blancs.
 
+OPBINAIRE : /[+*\/&><]/|"<="|">="|"-"|">>"
+VARIABLE : /(?!s)(?!struct_)[a-zA-Z_][a-zA-Z_0-9]*/ // interdit les varibles commençant par struct_ et par s
+ATTRIBUT : /[a-z]+/
+NOMBRE : SIGNED_NUMBER 
+STRUCT : "struct_" VARIABLE "_" VARIABLE // struct_C_X
+STRUCT_ATTRIBUT : STRUCT ("." ATTRIBUT)* // struct_C_X.a
+TYPE : "int " ATTRIBUT | "struct " VARIABLE " " ATTRIBUT // int a | struct C y
 VARIABLE_STRING : /[s][a-zA-Z0-9]*/
-VARIABLE : /[a-zA-Z_][a-zA-Z 0-9]*/
-NOMBRE : SIGNED_NUMBER
 STRING : ESCAPED_STRING
-OPBINAIRE: /[+*\/&><]/|">="|"-"|">>"
 
-expression : VARIABLE_STRING -> exp_variable_string
+expression : STRUCT -> exp_struct
+| STRUCT_ATTRIBUT -> exp_struct_attribut
+| VARIABLE_STRING -> exp_variable_string
 | VARIABLE -> exp_variable
 | NOMBRE -> exp_nombre
 | STRING -> exp_string
@@ -21,19 +27,31 @@ expression : VARIABLE_STRING -> exp_variable_string
 | "charAt" "(" VARIABLE_STRING "," (NOMBRE | VARIABLE) ")" -> exp_charat
 
 commande : VARIABLE "=" expression ";" -> com_asgt
+| STRUCT_ATTRIBUT "=" expression ";" -> com_assignation_struct_attribut // struct_C_X.a = 5;
 | "printf" "(" expression ")" ";" -> com_printf
 | commande+ -> com_sequence
 | "while" "(" expression ")" "{" commande "}" -> com_while
 | "if" "(" expression ")" "{" commande "}" "else" "{" commande "}" -> com_if
+| "struct " STRUCT ";" -> com_struct_declaration
+| "int " VARIABLE ";" -> com_int_declaration
 
 liste_var : -> liste_vide
 | (VARIABLE_STRING | VARIABLE) ("," (VARIABLE_STRING | VARIABLE))* -> liste_normale
 
-programme : "main" "(" liste_var ")" "{" commande "return" "(" expression ")" ";" "}" -> prog_main
+liste_attribut : -> liste_attribut_vide
+        | TYPE ("," TYPE)* -> liste_attribut_normale // int a, struct C y
+
+definition_struct : "struct" VARIABLE "{" liste_attribut "}" ";" -> def_struct // struct C {int a, struct C y} ;
+
+liste_def_struct : -> liste_def_struct_vide
+        | definition_struct+ -> liste_def_struct_normale
+
+programme : liste_def_struct "main" "(" liste_var ")" "{" commande "return" "(" expression ")" ";" "}" -> prog_main
 """
 
 parser = lark.Lark(grammaire, start="programme")
 
+'''
 t = parser.parse("""main(x,y){
                  while(x) {
                     y = y + 1;
@@ -54,39 +72,88 @@ t2 = parser.parse("""main(x, y, s1) {
                  return (y);
                 }
                  """)
+'''
+
+def pretty_printer_def_struct(t):
+    return "struct %s { %s };" % (t.children[0].value, pretty_printer_liste_attribut(t.children[1]))
+
+def pretty_printer_liste_def_struct(t):
+    if t.data == "liste_def_struct_vide":
+        return ""
+    return "\n".join([pretty_printer_def_struct(child) for child in t.children])
+
+def pretty_printer_programme(t):
+    def_struct = pretty_printer_liste_def_struct(t.children[0])
+    a = "main (%s) { \n %s \n return (%s);\n} " % (pretty_printer_liste_var(t.children[1]), 
+                                                pretty_printer_commande(t.children[2]), 
+                                                pretty_printer_expression(t.children[3]))
+    return f"{def_struct} \n{a}"
 
 def pretty_printer_liste_var(t):
     if t.data == "liste_vide":
         return ""
-    return ", ".join([u.value for u in t.children])
+    return ", ".join([child.value for child in t.children])
+
+def pretty_printer_liste_attribut(t):
+    if t.data == "liste_attribut_vide":
+        return ""
+    return ", ".join([child.value for child in t.children])
+    
+def pretty_printer_commande(t):
+    if t.data in ("com_assignation", "com_assignation_struct_attribut"):
+        return f"{t.children[0].value} = {pretty_printer_expression(t.children[1])};"
+    if t.data == "com_printf":
+        return f"printf({pretty_printer_expression(t.children[0])});"
+    if t.data == "com_sequence":
+        return "\n ".join([pretty_printer_commande(child) for child in t.children])
+    if t.data == "com_while":
+        return "while (%s) { \n %s \n }" % (pretty_printer_expression(t.children[0]), pretty_printer_commande(t.children[1]))
+    if t.data == "com_if":
+        return "if (%s) { \n %s \n } else { \n %s \n }" % (pretty_printer_expression(t.children[0]), pretty_printer_commande(t.children[1]), pretty_printer_commande(t.children[2]))
+    if t.data == "com_struct_declaration":
+        return f"struct {t.children[0]};"
+    if t.data == "com_int_declaration":
+        return f"int {t.children[0].value};"
 
 def pretty_printer_expression(t):
-    if t.data in ("exp_variable", "exp_nombre"):
+    if t.data in ("exp_variable", "exp_nombre", "exp_struct", "exp_struct_attribut"):
         return t.children[0].value
-    elif t.data == "exp_chaine":
-        return t.children[0].value
-    elif t.data == "exp_charat":
-        return f"charAt({t.children[0].value}, {t.children[1].value})"
+    if t.data == "exp_struct_attribut":
+        return pretty_printer_expression(t.children[0]) + "." + t.children[1].value
     return f"{pretty_printer_expression(t.children[0])} {t.children[1].value} {pretty_printer_expression(t.children[2])}"
 
-def pretty_printer_commande(t):
-    if t.data == "com_asgt":
-        return f"{t.children[0].value} = {pretty_printer_expression(t.children[1])} ;"
-    if t.data == "com_printf":
-        return f"printf ({pretty_printer_expression(t.children[0])}) ;"
-    if t.data == "com_while":
-        return "while (%s){ %s}" % (pretty_printer_expression(t.children[0]), pretty_printer_commande(t.children[1]))
-    if t.data == "com_if":
-        return "if (%s){ %s} else { %s}" % (pretty_printer_expression(t.children[0]), pretty_printer_commande(t.children[1]), pretty_printer_commande(t.children[2]))
-    if t.data == "com_sequence":
-        return "\n".join([pretty_printer_commande(u) for u in t.children])
 
-def pretty_print(t):
-    return "main (%s) { %s return (%s); }" % (
-        pretty_printer_liste_var(t.children[0]), 
-        pretty_printer_commande(t.children[1]),
-        pretty_printer_expression(t.children[2])
-    )
+
+'''
+
+parser = lark.Lark(grammaire, start="programme")
+parser2 = lark.Lark(grammaire, start="expression")
+
+final2 = parser.parse("""
+                 struct C {int a, int b} ;
+                 struct A {int a, struct C y} ;
+                 main(X, Z) {
+                    struct_C_X.a = 5;
+                    X = 6; 
+                    struct_A_Z.y = struct_C_X;
+                    return (struct_A_Z.y.b);
+                 }
+                 """)
+
+final = parser.parse("""
+                    struct C {int a, int b} ;
+                    struct A {int a, struct C y} ;
+                    main(X, Z) {
+                        struct struct_C_X;
+                        struct struct_A_Z;
+                        int j;
+                        struct_C_X.a = 5;
+                        struct_C_Z.y = struct_C_X;
+                        X = 6; 
+                        return (X);
+                    }
+                    """)
 
 #print(pretty_print(t))
 #print(t)
+'''
